7c4df3af540cde51628ae6910316eddd4b3b0bbb
Yagen1234 <walryx@mail.ru>
2025-02-08 17:15:19 +0300

    Create QuickSort

---
+++ src/main/java/SortDir/QuickSort.java
@@ -0,0 +1,42 @@
+package SortDir;
+
+import java.util.Comparator;
+
+public class QuickSort<T> {
+    private Comparator<T> comparator;
+
+    public QuickSort(Comparator<T> comparator) {
+        this.comparator = comparator;
+    }
+
+    public void sort(T[] array) {
+        quickSort(array, 0, array.length-1);
+    }
+
+    private void quickSort(T[] array, int low, int high) {
+        if(low < high) {
+            int pivotIndex = partition(array, low, high);
+            quickSort(array, low, pivotIndex-1);
+            quickSort(array, pivotIndex+1, high);
+        }
+    }
+
+    private  int partition(T[] array, int low, int high) {
+        T pivot = array[high];
+        int i = (low-1);
+        for(int j = low; j < high; j++) {
+            if(comparator.compare(array[j],pivot) <= 0) {
+                i++;
+                swap(array, i, j);
+            }
+        }
+        swap(array, i+1, high);
+        return i+1;
+    }
+
+    private void swap(T[] array, int i, int j) {
+        T temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
