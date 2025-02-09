92dbed0eb155fe2bd3e45116e7aa37e4c444ef3a
Panenkova Daria <dariapanenkova@gmail.com>
2025-02-09 16:50:14 +0300

    clear main

--- src/main/java/Client/Main.java
+++ src/main/java/Client/Main.java
@@ -1,10 +1,5 @@
 package Client;
 
-import DataClasses.Bus;
-import DataClasses.Student;
-import SortDir.quickSorts.QuickSortBus;
-import SortDir.quickSorts.QuickSortStudent;
-
 public class Main {
     public static void main(String[] args) {
 
