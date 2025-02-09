6d3ac325533cf2bc29bda557c9b9e150f049850a
lena <lenakochemarova@gmail.com>
2025-02-09 15:32:34 +0300

    fix

--- src/main/java/Client/Main.java
+++ src/main/java/Client/Main.java
@@ -1,21 +1,12 @@
 package Client;
 
 import DataClasses.Bus;
+import DataClasses.Student;
 import SortDir.quickSorts.QuickSortBus;
+import SortDir.quickSorts.QuickSortStudent;
 
 public class Main {
     public static void main(String[] args) {
-        Bus b1 =  new Bus.BusBuilder().setMileage(1000).setModel("abcc").setNum(100).build();
-        Bus b2 =  new Bus.BusBuilder().setMileage(100).setModel("abcc").setNum(100).build();
-        Bus b3 =  new Bus.BusBuilder().setMileage(100).setModel("aabcc").setNum(100).build();
-        Bus b4 =  new Bus.BusBuilder().setMileage(100).setModel("abcc").setNum(4).build();
-
-        Bus[] buses = new Bus[]{b1,b2,b3,b4};
-
-        new QuickSortBus().sort(buses);
-        System.out.println();
-
-
 
     }
 }
