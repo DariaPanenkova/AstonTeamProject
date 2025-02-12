85f255048df5e6b8613fd12d6370f49b64b38ee9
Лена <l@>
2025-02-12 16:08:42 +0300

    добавила в компораторы проверку на null

--- src/main/java/sortdir/comparators/BusComparator.java
+++ src/main/java/sortdir/comparators/BusComparator.java
@@ -7,10 +7,12 @@ import java.util.Comparator;
 public class BusComparator implements Comparator<Bus> {
     @Override
     public int compare(Bus o1, Bus o2) {
-        if (o1.getNum()-o2.getNum()!=0)
-            return o1.getNum()-o2.getNum();
-        else if ((o1.getModel().compareTo(o2.getModel())!=0))
-            return o1.getModel().compareTo(o2.getModel());
+        if (o1.getNum() - o2.getNum() != 0)
+            return o1.getNum() - o2.getNum();
+        else if (o1.getModel() != null && o2.getModel() != null)
+            if ((o1.getModel().compareTo(o2.getModel()) != 0))
+                return o1.getModel().compareTo(o2.getModel());
+            else return o1.getMileage() - o2.getMileage();
         else return o1.getMileage() - o2.getMileage();
     }
 }
