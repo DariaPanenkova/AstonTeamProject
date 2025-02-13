696f52ee05b6e546f74966f95b420646000368cb
Panenkova Daria <dariapanenkova@gmail.com>
2025-02-13 19:00:37 +0300

    Add action in ClientLoop

--- src/main/java/client/ClientLoop.java
+++ src/main/java/client/ClientLoop.java
@@ -2,8 +2,13 @@ package client;
 
 import arrays.CreateArray;
 import cycles.DataInputTypeCycle;
+import cycles.SearchCycle;
+import savetofile.SaveToFile;
+import sortdir.evensort.EvenSortData;
+import sortdir.quicksorts.DataQuickSort;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ClientLoop {
@@ -25,6 +30,13 @@ public class ClientLoop {
 
             try {
                 Object[] array = DataInputTypeCycle.inputData(CreateArray.createArray(type, size));
+
+                int action = actionArrayMenu(array);
+
+                if (action == 0) {
+                    break;
+                }
+
             } catch (NullPointerException | IOException e) {
                 System.out.println();
             }
@@ -65,6 +77,103 @@ public class ClientLoop {
             } else {
                 System.out.println("Недопустимый ввод. Пожалуйста, введите число.");
                 scanner.next(); // Очистка некорректного ввода
+            }
+        }
+    }
+
+    // Шаг вопрос сортировка или поиск?
+    private static int actionArrayMenu(Object[] array) {
+        Scanner scanner = new Scanner(System.in);
+        Object[] sortArray = DataQuickSort.sort(array);
+
+        while (true) {
+            System.out.print("Выберите действие (1 - Кастомная сортировка, 2 - Бинарный поиск, 3 - Создать новый массив, 0 - выход): ");
+            if (scanner.hasNextInt()) {
+                int choice = scanner.nextInt();
+                scanner.nextLine(); // Очистка буфера
+
+                switch (choice) {
+                    case 1:
+                        customSort(array);
+                        break;
+                    case 2:
+                        binarySearch(sortArray);
+                        break;
+                    case 3:
+                        return 1;
+                    case 0:
+                        System.out.println("Выход из программы.");
+                        return 0;
+                    default:
+                        System.out.println("Недопустимый ввод. Пожалуйста, выберите 0, 1, 2 или 3.");
+                }
+            } else {
+                System.out.println("Недопустимый ввод. Пожалуйста, введите число.");
+                scanner.nextLine(); // Очистка буфера
+            }
+        }
+    }
+
+    private static void customSort(Object[] array) {
+        if (array.length == 0) {
+            System.out.println("Массив пуст. Сначала создайте массив.");
+            return;
+        }
+
+        Object[] evenArr = EvenSortData.sort(array);
+
+        System.out.println("Отсортированный массив методом чётной сортировки:\n" + Arrays.toString(evenArr));
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("Сохранить отсортированный массив в файл? (1 - Да, 2 - Нет): ");
+            if (scanner.hasNextInt()) {
+                int choice = scanner.nextInt();
+                scanner.nextLine(); // Очистка буфера
+                switch (choice) {
+                    case 1:
+                        SaveToFile.getPathAndSave(evenArr);
+                        return;
+                    case 2:
+                        return;
+                    default:
+                        System.out.println("Недопустимый ввод. Пожалуйста, выберите 1, 2");
+                }
+            } else {
+                System.out.println("Недопустимый ввод. Пожалуйста, введите число.");
+                scanner.nextLine(); // Очистка буфера
+            }
+        }
+    }
+
+    private static void binarySearch(Object[] sortArray) {
+        if (sortArray.length == 0) {
+            System.out.println("Массив пуст. Сначала создайте массив.");
+            return;
+        }
+
+        SearchCycle<Object> search = new SearchCycle<>();
+
+        int index = search.search(sortArray);
+        Scanner scanner = new Scanner(System.in);
+        if (index != -1) {
+            while (true) {
+                System.out.print("Сохранить найденный элемент в файл? (1 - Да, 2 - Нет): ");
+                if (scanner.hasNextInt()) {
+                    int choice = scanner.nextInt();
+                    scanner.nextLine(); // Очистка буфера
+                    switch (choice) {
+                        case 1:
+                            SaveToFile.getPathAndSave(sortArray[index]);
+                            return;
+                        case 2:
+                            return;
+                        default:
+                            System.out.println("Недопустимый ввод. Пожалуйста, выберите 1, 2");
+                    }
+                } else {
+                    System.out.println("Недопустимый ввод. Пожалуйста, введите число.");
+                    scanner.nextLine(); // Очистка буфера
+                }
             }
         }
     }
