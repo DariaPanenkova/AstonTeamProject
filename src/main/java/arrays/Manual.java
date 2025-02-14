045ccaec3d66575dde3b309bceb72569418e3e65
Panenkova Daria <dariapanenkova@gmail.com>
2025-02-14 15:48:16 +0300

    Refactor manual loop

--- src/main/java/arrays/Manual.java
+++ src/main/java/arrays/Manual.java
@@ -9,124 +9,143 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Manual {
-    public static Bus[] createBus(Bus[] array) {
-        Scanner console = new Scanner(System.in);
+    public static Bus[] manualFillBusArray(Bus[] array) {
+
         for (int i = 0; i < array.length; i++) {
             System.out.println("Автобус " + (i + 1) + "\n");
-            System.out.println("Введите номер автобуса");
-            int num;
-            while (true) {
-                if (console.hasNextInt()) {
-                    num = console.nextInt();
-                    break;
-                } else {
-                    System.out.println("Введите число");
-                }
-                console.next();
-            }
-            console.nextLine();
-
-            System.out.println("Введите модель автобуса");
-            String model = console.nextLine();
-
-            int mileage;
-            while (true) {
-                System.out.println("Введите пробег автобуса");
-                if (console.hasNextInt()) {
-                    mileage = console.nextInt();
-                    break;
-                } else {
-                    System.out.println("Введите число");
-                }
-                console.next();
-            }
-            console.nextLine();
-            array[i] = new Bus.BusBuilder().setNum(num).setModel(model).setMileage(mileage).build();
+            array[i] = Manual.manualCreateBus();
         }
         System.out.println(Arrays.toString(array) + "\n");
         return array;
     }
 
-    public static User[] createUser(User[] array) {
+    public static Bus manualCreateBus(){
         Scanner console = new Scanner(System.in);
+
+        System.out.println("Введите номер автобуса");
+        int num;
+
+        while (true) {
+            if (console.hasNextInt()) {
+                num = console.nextInt();
+                break;
+            } else {
+                System.out.println("Введите число");
+            }
+            console.next();
+        }
+        console.nextLine();
+
+        System.out.println("Введите модель автобуса");
+        String model = console.nextLine();
+
+        int mileage;
+
+        while (true) {
+            System.out.println("Введите пробег автобуса");
+            if (console.hasNextInt()) {
+                mileage = console.nextInt();
+                break;
+            } else {
+                System.out.println("Введите число");
+            }
+            console.next();
+        }
+        console.nextLine();
+
+        return new Bus.BusBuilder().setNum(num).setModel(model).setMileage(mileage).build();
+    }
+
+    public static User[] manualFillUserArray(User[] array) {
+
         for (int i = 0; i < array.length; i++) {
             System.out.println("Пользователь " + (i + 1) + "\n");
-            String name;
-            String password;
-            String mail;
-            System.out.println("Введите имя пользователя");
-            while (true) {
-                Pattern namepattern = Pattern.compile("[0-9!@#$%^&*()_+|}{',./:]");
-                name = console.nextLine();
-                if (namepattern.matcher(name).find()) {
-                    System.out.println("Имя не должно сожержать цифр и знаков");
-                } else break;
-            }
-            System.out.println("Введите почту пользователя");
-            while (true) {
-                Pattern mailPattern = Pattern.compile("^[A-Za-z0-9+_.-]+@([A-Za-z0-9-]+\\.)+[A-Za-z]{2,6}$");
-                mail = console.nextLine();
-                if (!mailPattern.matcher(mail).find()) {
-                    System.out.println("Неверный формат почты");
-                } else break;
 
-            }
-            System.out.println("Введите пароль пользователя");
-            while (true) {
-                Pattern passwordPattern = Pattern.compile("^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[!@#$%^&*()]).{8,}$");
-                password = console.nextLine();
-                if (!passwordPattern.matcher(password).find()) {
-                    System.out.println("Пароль должен содержать 8 символов, одну заглавную, одну строчную букву, цифру, и спецсимвол");
-                } else break;
-            }
-            array[i] = new User.UserBuilder().setName(name).setPassword(password).setMail(mail).createUser();
+            array[i] = manualCreateUser();
 
         }
         System.out.println(Arrays.toString(array) + "\n");
         return array;
     }
 
-    public static Student[] createStudent(Student[] array) {
+    public static User manualCreateUser(){
         Scanner console = new Scanner(System.in);
-        console.useDelimiter("\n");
+        String name;
+        String password;
+        String mail;
+        System.out.println("Введите имя пользователя");
+        while (true) {
+            Pattern namepattern = Pattern.compile("[0-9!@#$%^&*()_+|}{',./:]");
+            name = console.nextLine();
+            if (namepattern.matcher(name).find()) {
+                System.out.println("Имя не должно сожержать цифр и знаков");
+            } else break;
+        }
+        System.out.println("Введите почту пользователя");
+        while (true) {
+            Pattern mailPattern = Pattern.compile("^[A-Za-z0-9+_.-]+@([A-Za-z0-9-]+\\.)+[A-Za-z]{2,6}$");
+            mail = console.nextLine();
+            if (!mailPattern.matcher(mail).find()) {
+                System.out.println("Неверный формат почты");
+            } else break;
+
+        }
+        System.out.println("Введите пароль пользователя");
+        while (true) {
+            Pattern passwordPattern = Pattern.compile("^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[!@#$%^&*()]).{8,}$");
+            password = console.nextLine();
+            if (!passwordPattern.matcher(password).find()) {
+                System.out.println("Пароль должен содержать 8 символов, одну заглавную, одну строчную букву, цифру, и спецсимвол");
+            } else break;
+        }
+
+        return new User.UserBuilder().setName(name).setPassword(password).setMail(mail).createUser();
+    }
+
+    public static Student[] manualFillStudentArray(Student[] array) {
         for (int i = 0; i < array.length; i++) {
             System.out.println("Студент " + (i + 1) + "\n");
-            System.out.println("Введите номер зачётной книжки студента");
-            int gradeBookNum;
-            while (true) {
-                if (console.hasNextInt()) {
-                    gradeBookNum = console.nextInt();
-                    if (gradeBookNum > 0) {
-                        break;
-                    } else System.out.println("Номер зачётной книжки должен быть больше 0");
-                } else {
-                    System.out.println("Введите число");
-                    console.next();
-                }
-            }
-            console.nextLine();
-
-            System.out.println("Введите группу студента");
-            String group = console.nextLine();
-
-            double averageGrade;
-            while (true) {
-                System.out.println("Введите среднюю оценку студента");
-                if (console.hasNextDouble()) {
-                    averageGrade = console.nextDouble();
-                    if (averageGrade > 0) {
-                        break;
-                    } else System.out.println("Средняя оценка должна быть больше 0");
-
-                } else {
-                    System.out.println("Введите число");
-                }
-                console.next();
-            }
-            console.nextLine();
-            array[i] = new Student.StudentBuilder().setGradeBookNum(gradeBookNum).setGroup(group).setAverageGrade(averageGrade).build();
+            array[i] = manualCreateStudent();
         }
         System.out.println(Arrays.toString(array) + "\n");
         return array;
+    }
+    public static Student manualCreateStudent() {
+        Scanner console = new Scanner(System.in);
+        console.useDelimiter("\n");
+        System.out.println("Введите номер зачётной книжки студента");
+        int gradeBookNum;
+        while (true) {
+            if (console.hasNextInt()) {
+                gradeBookNum = console.nextInt();
+                if (gradeBookNum > 0) {
+                    break;
+                } else System.out.println("Номер зачётной книжки должен быть больше 0");
+            } else {
+                System.out.println("Введите число");
+                console.next();
+            }
+        }
+        console.nextLine();
+
+        System.out.println("Введите группу студента");
+        String group = console.nextLine();
+
+        double averageGrade;
+        while (true) {
+            System.out.println("Введите среднюю оценку студента");
+            if (console.hasNextDouble()) {
+                averageGrade = console.nextDouble();
+                if (averageGrade > 0) {
+                    break;
+                } else System.out.println("Средняя оценка должна быть больше 0");
+
+            } else {
+                System.out.println("Введите число");
+            }
+            console.next();
+        }
+        console.nextLine();
+        return new Student.StudentBuilder().setGradeBookNum(gradeBookNum).setGroup(group).setAverageGrade(averageGrade).build();
     }
 }
--- src/main/java/client/BinarySearchLoop.java
+++ src/main/java/client/BinarySearchLoop.java
@@ -1,5 +1,6 @@
 package client;
 
+import arrays.Manual;
 import binarysearch.DataBinarySearch;
 import dataclasses.Bus;
 import dataclasses.Student;
@@ -11,54 +12,22 @@ public class BinarySearchLoop<T> {
 
     public int search(T[] array) {
         Scanner console = new Scanner(System.in);
+
         if (array instanceof Bus[]) {
-            while (true) {
-                try {
-                    System.out.println("Введите номер искомого автобуса");
-                    int num = Integer.parseInt(console.nextLine());
-                    System.out.println("Введите модель искомого автобуса");
-                    String model = console.nextLine();
-                    System.out.println("Введите пробег искомого автобуса");
-                    int mileage = Integer.parseInt(console.nextLine());
-                    Bus key = new Bus.BusBuilder().setNum(num).setModel(model).setMileage(mileage).build();
-                    return DataBinarySearch.binarySearch(array, key);
-                } catch (NumberFormatException e) {
-                    System.out.println("Неверный формат ввода");
-                }
-            }
+            Bus key = Manual.manualCreateBus();
+            return DataBinarySearch.binarySearch(array, key);
         }
+
         if (array instanceof Student[]) {
-            while (true) {
-                try {
-                    System.out.println("Введите номер зачётной книжки искомого студента");
-                    int gradeBookNum = Integer.parseInt(console.nextLine());
-                    System.out.println("Введите группу искомого студента");
-                    String group = console.nextLine();
-                    System.out.println("Введите средний балл искомого студента");
-                    double averageGrade = Double.parseDouble(console.nextLine());
-                    Student key = new Student.StudentBuilder().setGradeBookNum(gradeBookNum).setGroup(group).setAverageGrade(averageGrade).build();
-                    return DataBinarySearch.binarySearch(array, key);
-                } catch (NumberFormatException e) {
-                    System.out.println("Неверный формат ввода");
-                }
-            }
+            Student key = Manual.manualCreateStudent();
+            return DataBinarySearch.binarySearch(array, key);
         }
+
         if (array instanceof User[]) {
-            while (true) {
-                try {
-                    System.out.println("Введите имя искомого пользователя");
-                    String name = console.nextLine();
-                    System.out.println("Введите пароль искомого пользователя");
-                    String password = console.nextLine();
-                    System.out.println("Введите почту искомого пользователя");
-                    String mail = console.nextLine();
-                    User key = new User.UserBuilder().setName(name).setPassword(password).setMail(mail).createUser();
-                    return DataBinarySearch.binarySearch(array, key);
-                } catch (Exception e) {
-                    System.out.println("Неверный формат ввода");
-                }
-            }
+            User key = Manual.manualCreateUser();
+            return DataBinarySearch.binarySearch(array, key);
         }
+
         return -10;
     }
 }
